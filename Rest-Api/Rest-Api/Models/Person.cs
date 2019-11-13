93a78c34aca4475a737332e12068318e89117375
LeseleM <LeseleM@DESKTOP-ARKV6OM>
2019-11-13 09:06:17 +0200

    the commit

---
+++ Rest-Api/Rest-Api/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Api.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
