9a140a0ebd5e62a4ff1be3144362dbcc3dc23fe1
Lesele Mannathoko <Lesele Mannathoko@DESKTOP-S34IRVS>
2020-05-25 09:47:56 +0200

    the fiest skelton

---
+++ application-UI/src/app/Person/Person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Person',
+  templateUrl: './Person.component.html',
+  styleUrls: ['./Person.component.css']
+})
+export class PersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
